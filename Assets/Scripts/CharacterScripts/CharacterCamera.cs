7338a8f9930f6776ef82eb44d6bf76c11c4c636c
victo <victo@DESKTOP-5RH1MK1>
2017-10-15 17:30:35 -0200

    Simple camera and movement scripts

---
+++ Assets/Scripts/CharacterScripts/CharacterCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CharacterCamera : MonoBehaviour {
+
+    [SerializeField]
+    private Transform playerTransform;
+
+    [SerializeField]
+    private float mouseSensitivity = 2f;
+
+    [SerializeField]
+    private float distance = 30;
+
+    private float yaw;    
+    private Vector3 cameraDistance;    
+
+	void Start ()
+    {
+        cameraDistance = playerTransform.position - transform.position;
+    }
+		
+	void FixedUpdate ()
+    {
+        yaw = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        transform.RotateAround(playerTransform.position, Vector3.up, yaw);        
+        transform.position = playerTransform.position - transform.forward * distance;        
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(playerTransform.position, transform.position);
+    }
+
+}
---
+++ Assets/Scripts/CharacterScripts/CharacterMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 10f;
+
+    private Rigidbody rb;
+    private Transform cameraT;
+    private Animator anim;
+    private float turnSmoothVelocity;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        cameraT = Camera.main.transform;
+	}
+	
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 input = new Vector2(horizontalInput, verticalInput);
+        float angleRotation = (Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg) + cameraT.eulerAngles.y;
+        if(input.magnitude > 0)
+        {
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, angleRotation, ref turnSmoothVelocity, 0.5f);
+        }        
+        Vector3 movement = transform.forward * speed * input.magnitude * Time.deltaTime;        
+        rb.MovePosition(rb.position + movement);
+        anim.SetFloat("velocity", movement.magnitude);
+    }
+	
+}
