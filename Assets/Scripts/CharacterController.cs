4984570569164657d08f45eaf198f19265be49c2
victo <victo@DESKTOP-5RH1MK1>
2017-10-15 01:39:22 -0200

    init

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Animator anim;
+
+    public Rigidbody RigidBody { get { return rb; } }
+    public Animator Animator { get { return anim; } }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+	
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 10f;
+
+    private Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	
+}
